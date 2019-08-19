2989aa117c7f8068a1adcfcad832ebf3455ba170
Krazeus <anthony-garcia1@hotmail.com>
2019-08-19 01:19:30 -0500

    1

---
+++ WebServices/WebServices/WS_Entradas.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WS_Entradas.asmx.cs" Class="WebServices.WS_Entradas" %>
---
+++ WebServices/WebServices/WS_Entradas.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServices
+{
+    /// <summary>
+    /// Descripción breve de WS_Entradas
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WS_Entradas : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string VendoH()
+        {
+            return "a 10 el palo ";
+        }
+    }
+}
